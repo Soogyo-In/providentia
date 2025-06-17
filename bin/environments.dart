3296c39125a432f8cf1ac381957f0bbcec49e52e
Soogyo <sgin0821@icloud.com>
2025-06-17 22:35:31 +0900

    fix: credential 파일을 define 으로 읽어오지 못하는 오류 수정

--- bin/environments.dart
+++ bin/environments.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+
 const projectId = String.fromEnvironment('GCP_PROJECT_ID');
-const credentials = String.fromEnvironment('GOOGLE_APPLICATION_CREDENTIALS');
+final credentials = utf8.decode(
+  base64.decode(
+    String.fromEnvironment('GOOGLE_APPLICATION_CREDENTIALS_BASE64'),
+  ),
+);
 const serviceUrl = String.fromEnvironment('SERVICE_URL');
-const webhookToken = String.fromEnvironment('WEBHOOK_TOKEN');
+const googleDriveChannelToken = String.fromEnvironment(
+  'GOOGLE_DRIVE_CHANNEL_TOKEN',
+);
